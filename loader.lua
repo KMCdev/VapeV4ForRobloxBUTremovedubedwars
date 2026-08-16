2f366e754e1602fc34a07abf316065374439e530
KMCdev <idkyoidkyo945@gmail.com>
2026-08-16 00:03:03 -0500

    Update download URL in downloadFile function

--- loader.lua
+++ loader.lua
@@ -32,7 +32,7 @@ __modules = {
 		local function downloadFile(path, func)
 			if not isfile(path) then
 				local suc, res = pcall(function()
-					return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeCompiled/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+					return game:HttpGet('https://raw.githubusercontent.com/KMCdev/VapeV4ForRobloxBUTremovedubedwars/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 				end)
 				if not suc or res == '404: Not Found' then
 					error(res)
